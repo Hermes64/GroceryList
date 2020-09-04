6f173fe5bad32262a785a001efcab3fc35d8c35d
laubreilev <64140190+laubreilev@users.noreply.github.com>
2020-09-04 09:21:55 +0200

    Add files via upload

---
+++ GroceryList/groceryList/src/GroceryItemOrder.java
@@ -0,0 +1,11 @@
+public class GroceryItemOrder {
+    String name;
+    double price;
+    int amount;
+    public GroceryItemOrder(String name, int price) {
+        this.price = price;
+        this.name = name;
+        this.amount = amount;
+
+    }
+}
---
+++ GroceryList/groceryList/src/GroceryList.java
@@ -0,0 +1,11 @@
+public class GroceryList {
+
+
+    private GroceryList[] products = new GroceryList[10];
+    GroceryItemOrder product1 = new GroceryItemOrder("Æble", 10);
+    GroceryItemOrder product2 = new GroceryItemOrder("Brød", 10);
+
+
+
+
+}
