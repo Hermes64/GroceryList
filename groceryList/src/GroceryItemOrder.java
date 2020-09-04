434ee7f509273c91c681f9d618d704b39b50bafb
laubreilev <64140190+laubreilev@users.noreply.github.com>
2020-09-04 13:45:42 +0200

    GroceryList/GroceryItemOrder class opdateret
    getTotalCost, addGroceryToList, arrayCheckIfFull lavet i GroceryList
    get/set, lavet i GroceryItemOrder

--- groceryList/src/GroceryItemOrder.java
+++ groceryList/src/GroceryItemOrder.java
@@ -1,13 +1,33 @@
 public class GroceryItemOrder {
-    String name;
-    double price;
-    int amount;
-    int id;
-    public GroceryItemOrder(String name, double price, int amount, int id) {
+    private String name;
+    private double price;
+    private int amount;
+    private int id;
+
+    public GroceryItemOrder(String name, double price, int amount, int id) { // Constructor
         this.price = price;
         this.name = name;
         this.id = id;
         this.amount = amount;
+    }
 
+
+    public double getPrice() { // returnerer pris for mængden af en vare
+        return price*amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    public int getId() {
+        return id;
     }
 }
--- groceryList/src/GroceryList.java
+++ groceryList/src/GroceryList.java
@@ -7,15 +7,15 @@ public class GroceryList {
     private GroceryItemOrder[] totalProducts = new GroceryItemOrder[20];
 
     //Objekter af produkter som skal kunne lægges i indkøbslisten
-    GroceryItemOrder product1 = new GroceryItemOrder("Æble", 10, 1, 1);
+    GroceryItemOrder product1 = new GroceryItemOrder("Frugt", 10, 1, 1);
     GroceryItemOrder product2 = new GroceryItemOrder("Brød", 10,1,2);
-    GroceryItemOrder product3 = new GroceryItemOrder("Æg",10,1,3);
-    GroceryItemOrder product4 = new GroceryItemOrder("Æg",10,1,4);
-    GroceryItemOrder product5 = new GroceryItemOrder("Æg",10,1,5);
-    GroceryItemOrder product6 = new GroceryItemOrder("Æg",10,1,6);
-    GroceryItemOrder product7 = new GroceryItemOrder("Æg",10,1,7);
-    GroceryItemOrder product8 = new GroceryItemOrder("Æg",10,1,8);
-    GroceryItemOrder product9 = new GroceryItemOrder("Æg",10,1,9);
+    GroceryItemOrder product3 = new GroceryItemOrder("Müsli",20,1,3);
+    GroceryItemOrder product4 = new GroceryItemOrder("Mælk",10,1,4);
+    GroceryItemOrder product5 = new GroceryItemOrder("Juice",10,1,5);
+    GroceryItemOrder product6 = new GroceryItemOrder("Baguette",10,1,6);
+    GroceryItemOrder product7 = new GroceryItemOrder("Rødvin",200,1,7);
+    GroceryItemOrder product8 = new GroceryItemOrder("Napoleonshatte",10,1,8);
+    GroceryItemOrder product9 = new GroceryItemOrder("Snegle",20,1,9);
 
 
     public void setTotalProducts(){
@@ -32,23 +32,37 @@ public class GroceryList {
 
     //Metode til at vælge et objekt ud fra id der skal tilføjes liste array
     public void addGroceryToList(){
-        int i = 0;
+        int i = 0; // i = pladsen i listens array
+        if (arrayCheckIfFull()){
+            System.out.println("Der er ikke plads til mere på indkøbslisten!");
+            return;
+        }
         System.out.println("Indtast ID");
-        int id = scanner.nextInt();
+        int id = scanner.nextInt() -1; // -1 tager højde for at array starter på 0
         System.out.println("Indtast mængde");
         int amount = scanner.nextInt();
-        totalProducts[id-1].amount = amount;
-        totalProducts[id-1] = products[i];
+        amount = totalProducts[id-1].getAmount();
+        totalProducts[id].setAmount(amount); // ændrer mængde
+        products[i] = totalProducts[id]; // tager valgte objekt fra vare array plads og kloner den til liste array
         i++;
     }
+
     public double getTotalCost(){ //skal regne priser på varer sammen
         double totalCost = 0;
         for (int i = 0; i < products.length; i++) {
-            totalCost += products[i].price;
+            totalCost += products[i].getPrice();
 
         }
         return totalCost;
 
     }
-    // todo: lav metode til at give true hvis der er pladser tilbage på indkøbslisten (products)
+    public boolean arrayCheckIfFull() { // loop til at finde tomme pladser på indkøbslisten
+        for (int i = 0; i < products.length; i++) {
+            if (products[i] == null) {
+                return true;
+            }
+        }
+     return false;
+    }
+
 }
